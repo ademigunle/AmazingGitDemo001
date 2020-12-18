a32624d105dfb7a919c38b709b05b5ab25d93d31
Ademigunle <Potters@17>
2020-12-18 15:45:27 +0000

    Adding my three new classes

---
+++ src/Application2.java
@@ -0,0 +1,5 @@
+public class Application2 {
+    public void Iamofftothemarket(){
+        System.out.println("I am off to the market");
+    }
+}
---
+++ src/Mathematics.java
@@ -0,0 +1,8 @@
+public class Mathematics {
+    int Y=5;
+    int X=10;
+    int Z=Y*X;
+    public int getZ;
+
+
+}
